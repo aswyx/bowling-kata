66351a139b796ad9ec9988f73ef7d42700c334bd
aswyx <hornyik.laszlo@netpositive.hu>
2012-10-08 22:10:58 +0200

    tests refactor + last test added

--- resources/tests.js
+++ resources/tests.js
@@ -1,50 +1,113 @@
+/**
+ * make rolls to the game
+ *
+ * @param game
+ * @param quantity {Integer}
+ * @param value {Integer}
+ */
+function makeRoll(game, quantity, value) {
+    'use strict';
+    var i = 0;
+    for (i; i < quantity; i += 1) {
+        game.roll(value);
+    }
+}
+
+/**
+ * Tests a game with all one rolls
+ */
 test("testAllOnes", function () {
     "use strict";
-    var game = new Game(), i = 0;
-    for (i; i < 20; i += 1) {
-        game.roll(1);
-    }
+    var game = new Game();
+
+    makeRoll(game, 20, 1);
+
     deepEqual(game.getScore(), 20, "All ones game passed!");
 });
 
+/**
+ * Tests a game with all strike rolls
+ */
 test("testAllStrikes", function () {
     'use strict';
-    var game = new Game(), i = 0;
-    for (i; i < 12; i += 1) {
-        game.roll(10);
-    }
+    var game = new Game();
+
+    makeRoll(game, 12, 10);
+
     deepEqual(game.getScore(), 300, "All strikes game passed!");
 });
 
+/**
+ * Tests a gem with all spare frames
+ */
 test("testAllSpares", function () {
     'use strict';
-    var game = new Game(), i = 0;
-    for (i; i < 21; i += 1) {
-        game.roll(5);
-    }
+    var game = new Game();
+
+    makeRoll(game, 21, 5);
+
     deepEqual(game.getScore(), 150, "All spares game passed!");
 });
 
+/**
+ * Tests a game with all zero rolls
+ */
 test("testAllGutters", function () {
     'use strict';
-    var game = new Game(), i = 0;
-    for (i; i < 21; i += 1) {
-        game.roll(0);
-    }
+    var game = new Game();
+
+    makeRoll(game, 20, 0);
+
     deepEqual(game.getScore(), 0, "All gutters game passed!");
 });
 
+/**
+ * Tests a gem with 5 strikes, 2 gutters and 5 strikes again
+ */
 test("testTwoGutters", function () {
     'use strict';
     var game = new Game(), i = 0;
-    for (i; i < 5; i += 1) {
-        game.roll(10);
-    }
-    for (i = 0; i < 4; i += 1) {
-        game.roll(0);
-    }
-    for (i = 0; i < 5; i += 1) {
-        game.roll(10);
-    }
+
+    makeRoll(game, 5, 10);
+    makeRoll(game, 4, 0);
+    makeRoll(game, 5, 10);
+
     deepEqual(game.getScore(), 210, "Strikes with two gutter frames passed!");
+});
+
+/**
+ * Tests a gem with no pattern
+ */
+test("testRandom", function () {
+    'use strict';
+    var game = new Game();
+    makeRoll(game, 1, 4);
+    makeRoll(game, 1, 2);
+
+    makeRoll(game, 1, 0);
+    makeRoll(game, 1, 5);
+
+    makeRoll(game, 1, 10);
+
+    makeRoll(game, 1, 7);
+    makeRoll(game, 1, 2);
+
+    makeRoll(game, 1, 0);
+    makeRoll(game, 1, 5);
+
+    makeRoll(game, 1, 5);
+    makeRoll(game, 1, 2);
+
+    makeRoll(game, 2, 0);
+
+    makeRoll(game, 1, 3);
+    makeRoll(game, 1, 5);
+
+    makeRoll(game, 2, 5);
+
+    makeRoll(game, 1, 2);
+    makeRoll(game, 1, 6);
+
+
+    deepEqual(game.getScore(), 79, "Game without pattern passed!");
 });
