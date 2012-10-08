831e779b93964ca3ace2b663a9079b8bf1ee7b50
aswyx <hornyik.laszlo@netpositive.hu>
2012-10-08 21:49:17 +0200

    two gutters thest passed + removed unnecessary first test

--- BowlingGame.js
+++ BowlingGame.js
@@ -12,6 +12,7 @@ function Game() {
  */
 Game.prototype.roll = function (hit) {
     "use strict";
+    console.log(hit);
     this.rolls.push(hit);
 };
 
@@ -22,16 +23,21 @@ Game.prototype.roll = function (hit) {
  */
 Game.prototype.getScore = function () {
     "use strict";
+    console.log(this.rolls);
     var score = 0, i = 0, frame = 0;
-    for (i; this.rolls[i] !== undefined; i += 1) {
-        score += this.rolls[i];
-        if (this.rolls[i] === 10 && frame < 9) {
-            score += this.rolls[i + 1] + this.rolls[i + 2];
-            frame += 1;
-        } else if (this.rolls[i] + this.rolls[i - 1] === 10 && frame < 9) {
-            score +=  this.rolls[i + 1];
-            frame += 1;
+    while (this.rolls[i] !== undefined && frame < 10) {
+        if (this.rolls[i] === 10) {
+            score += this.rolls[i] + this.rolls[i + 1] + this.rolls[i + 2];
+            i += 1;
+        } else if (this.rolls[i] + this.rolls[i + 1] === 10) {
+            score += this.rolls[i] + this.rolls[i + 1] + this.rolls[i + 2];
+            i += 2;
+        } else {
+            score += this.rolls[i] + this.rolls[i + 1];
+            i += 2;
         }
+        //console.log(score);
+        frame += 1;
     }
     return score;
 };
--- resources/tests.js
+++ resources/tests.js
@@ -1,14 +1,7 @@
-test("firstTest", function () {
-    "use strict";
-    var game = new Game();
-    game.roll(1);
-    ok(1 === game.getScore(), "Passed!");
-});
-
 test("testAllOnes", function () {
     "use strict";
-    var game = new Game(), i;
-    for (i = 0; i < 20; i += 1) {
+    var game = new Game(), i = 0;
+    for (i; i < 20; i += 1) {
         game.roll(1);
     }
     deepEqual(game.getScore(), 20, "All ones game passed!");
@@ -16,8 +9,8 @@ test("testAllOnes", function () {
 
 test("testAllStrikes", function () {
     'use strict';
-    var game = new Game(), i;
-    for (i = 0; i < 12; i += 1) {
+    var game = new Game(), i = 0;
+    for (i; i < 12; i += 1) {
         game.roll(10);
     }
     deepEqual(game.getScore(), 300, "All strikes game passed!");
@@ -25,8 +18,8 @@ test("testAllStrikes", function () {
 
 test("testAllSpares", function () {
     'use strict';
-    var game = new Game(), i;
-    for (i = 0; i < 21; i += 1) {
+    var game = new Game(), i = 0;
+    for (i; i < 21; i += 1) {
         game.roll(5);
     }
     deepEqual(game.getScore(), 150, "All spares game passed!");
@@ -34,9 +27,24 @@ test("testAllSpares", function () {
 
 test("testAllGutters", function () {
     'use strict';
-    var game = new Game(), i;
-    for (i = 0; i < 21; i += 1) {
+    var game = new Game(), i = 0;
+    for (i; i < 21; i += 1) {
         game.roll(0);
     }
     deepEqual(game.getScore(), 0, "All gutters game passed!");
+});
+
+test("testTwoGutters", function () {
+    'use strict';
+    var game = new Game(), i = 0;
+    for (i; i < 5; i += 1) {
+        game.roll(10);
+    }
+    for (i = 0; i < 4; i += 1) {
+        game.roll(0);
+    }
+    for (i = 0; i < 5; i += 1) {
+        game.roll(10);
+    }
+    deepEqual(game.getScore(), 210, "Strikes with two gutter frames passed!");
 });
