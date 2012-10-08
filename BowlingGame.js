74a1fcc265075284467db8d2053360650c0d51c7
aswyx <hornyik.laszlo@netpositive.hu>
2012-10-08 21:09:17 +0200

    all strike rolls test passed

--- BowlingGame.js
+++ BowlingGame.js
@@ -4,15 +4,15 @@
  */
 function Game() {
     "use strict";
-    this.score = 0;
+    this.rolls = [];
 }
 
 /**
- * rolls a ball
+ * adds the number of hit pins to rolls array
  */
 Game.prototype.roll = function (hit) {
     "use strict";
-    this.score += hit;
+    this.rolls.push(hit);
 };
 
 /**
@@ -22,5 +22,13 @@ Game.prototype.roll = function (hit) {
  */
 Game.prototype.getScore = function () {
     "use strict";
-    return this.score;
+    var score = 0, i = 0, frame = 0;
+    for (i; this.rolls[i] !== undefined; i += 1) {
+        score += this.rolls[i];
+        if (this.rolls[i] === 10 && frame < 9) {
+            score += this.rolls[i + 1] + this.rolls[i + 2];
+            frame += 1;
+        }
+    }
+    return score;
 };
--- resources/tests.js
+++ resources/tests.js
@@ -5,11 +5,20 @@ test("firstTest", function () {
     ok(1 === game.getScore(), "Passed!");
 });
 
-test( "testAllOnes", function() {
+test("testAllOnes", function () {
     "use strict";
     var game = new Game(), i;
     for (i = 0; i < 20; i += 1) {
         game.roll(1);
     }
     deepEqual(game.getScore(), 20, "All ones game passed!");
+});
+
+test("testAllStrikes", function () {
+    'use strict';
+    var game = new Game(), i;
+    for (i = 0; i < 12; i += 1) {
+        game.roll(10);
+    }
+    deepEqual(game.getScore(), 300, "All strike game passed!");
 });
