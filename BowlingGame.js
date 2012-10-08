847d139ef4f4f46809e1a372760bdba2f1d66669
aswyx <hornyik.laszlo@netpositive.hu>
2012-10-08 22:44:33 +0200

    some refactoring for readability

--- BowlingGame.js
+++ BowlingGame.js
@@ -16,6 +16,28 @@ Game.prototype.roll = function (hit) {
 };
 
 /**
+ * checks if a roll is a strike
+ *
+ * @param rollIndex {Integer}
+ * @return {Boolean}
+ */
+Game.prototype.isStrike = function (rollIndex) {
+    "use strict";
+    return this.rolls[rollIndex] === 10;
+};
+
+/**
+ * checks if a roll is a spare
+ *
+ * @param rollIndex {Integer}
+ * @return {Boolean}
+ */
+Game.prototype.isSpare = function (rollIndex) {
+    "use strict";
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
+};
+
+/**
  * calculates the score
  *
  * while trying to pass the two gutters test, i realized i need a different approach - instead of looking back
@@ -27,10 +49,10 @@ Game.prototype.getScore = function () {
     "use strict";
     var score = 0, rollIndex = 0, frameIndex = 0;
     while (this.rolls[rollIndex] !== undefined && frameIndex < 10) {
-        if (this.rolls[rollIndex] === 10) {
+        if (this.isStrike(rollIndex)) {
             score += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
             rollIndex += 1;
-        } else if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10) {
+        } else if (this.isSpare(rollIndex)) {
             score += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
             rollIndex += 2;
         } else {
